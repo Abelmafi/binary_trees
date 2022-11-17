befa9534de532fd55db8967a96d8b3274a7f7848
abel mesfin <abelmesfin555@gmail.com>
2022-11-17 15:23:31 +0300

    output error fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent)
+		return (NULL);
 	new_node = binary_tree_node(parent, value);
-	if (!parent || !new_node)
+	if (!new_node)
 		return (NULL);
 	if (parent->left)
 	{
