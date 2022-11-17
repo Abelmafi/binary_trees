50631860409072a7ac53e93d7a2e7cb23b19e541
abel mesfin <abelmesfin555@gmail.com>
2022-11-17 15:20:20 +0300

    betty linter error fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_l = 1 + binary_tree_height(tree->left);
 	height_r = 1 + binary_tree_height(tree->right);
 	if (height_l > height_r)
-	       	return (height_l);
+		return (height_l);
 	return (height_r);
 }
