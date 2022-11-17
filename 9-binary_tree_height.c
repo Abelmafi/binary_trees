58a27df69d5b2156897388de59d722d9033d403c
abel mesfin <abelmesfin555@gmail.com>
2022-11-17 15:18:27 +0300

    output error fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0, height_l = 0, height_r = 0;
+	size_t height_l = 0, height_r = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	height_l = binary_tree_height(tree->left);
-	height_r = binary_tree_height(tree->right);
-	height = height_l > height_r ? height_l + 1 : height_r + 1;
-	return (height);
+	height_l = 1 + binary_tree_height(tree->left);
+	height_r = 1 + binary_tree_height(tree->right);
+	if (height_l > height_r)
+	       	return (height_l);
+	return (height_r);
 }
