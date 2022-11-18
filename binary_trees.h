6647f3615fe78861f898464ea401c229dd02961a
abel mesfin <abelmesfin555@gmail.com>
2022-11-18 21:42:44 +0300

    output error fix

--- binary_trees.h
+++ binary_trees.h
@@ -64,4 +64,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int btih_helper(const binary_tree_t *tree);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 #endif
